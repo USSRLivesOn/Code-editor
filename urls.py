1dd522e36e0a8270ce2290ac397804970d89b5fc
Stan Mishchenko <USSRLivesOn@gmail.com>
2011-01-22 13:22:03 -0500

    updated urls to not require editor prefix

--- urls.py
+++ urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^editor/$', 'editor.js_play.views.index'),
-	(r'^editor/ajax_save_file/$', 'editor.js_play.views.ajax_save_file'),
-	(r'^editor/ajax_get_file/$', 'editor.js_play.views.ajax_get_file'),
+    (r'^/$', 'editor.js_play.views.index'),
+	(r'^/ajax_save_file/$', 'editor.js_play.views.ajax_save_file'),
+	(r'^/ajax_get_file/$', 'editor.js_play.views.ajax_get_file'),
 	#(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     #(r'^admin/', include(admin.site.urls)),
 	#(r'^css/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/Users/ussrliveson/Sites/editor/css'}),
